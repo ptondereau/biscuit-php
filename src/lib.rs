44d23683445ffebb011853f42386411bb753b614
alyrow <58048578+alyrow@users.noreply.github.com>
2023-05-29 14:39:52 +0200

    fix clippy errors

--- src/lib.rs
+++ src/lib.rs
@@ -227,7 +227,7 @@ impl BiscuitBuilder {
         self.0
             .clone()
             .build(&root_key.0)
-            .and_then(|b| Ok(Biscuit(b)))
+            .map(Biscuit)
             .map_err(|e| {
                 PhpException::new(e.to_string(), 0, unsafe {
                     INVALID_TERM.expect("did not set exception ce")
